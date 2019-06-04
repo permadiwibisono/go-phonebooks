8e15e5cd778278f955ab6a791798669c3e74fee2
Permadi Wibisono <mbapewe@gmail.com>
2019-06-04 17:09:06 +0700

    Added RespondError func utils

--- utils/response.go
+++ utils/response.go
@@ -6,10 +6,21 @@ import (
 )
 
 func Message(status int, msg string) map[string]interface{} {
-	return map[string]interface{}{"status": status, "message": msg}
+	return map[string]interface{}{"status_code": status, "message": msg}
 }
 
-func Respond(w http.ResponseWriter, data map[string]interface{}) {
+func MessageWithData(status int, msg string, data map[string]interface{}) map[string]interface{} {
+	return map[string]interface{}{"status_code": status, "message": msg, "data": data}
+}
+
+func Respond(w http.ResponseWriter, statusCode int, data map[string]interface{}) {
 	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(data)
+}
+
+func RespondError(w http.ResponseWriter, statusCode int, msg string, errData map[string]interface{}) {
+	errData["status_code"] = statusCode
+	errData["message"] = msg
+	Respond(w, statusCode, errData)
 }
