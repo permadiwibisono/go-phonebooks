d38f5a36fb16abc74a354682696ff691d8cad49b
Permadi Wibisono <mbapewe@gmail.com>
2019-06-07 08:32:16 +0700

    Setting routes and how to assign custom middleware in spesific request

--- main.go
+++ main.go
@@ -5,34 +5,96 @@ import (
 	"net/http"
 	"os"
 
-	middlewares "go-phonebooks/middlewares"
-	"go-phonebooks/models"
-	res "go-phonebooks/utils"
+	"go-phonebooks/controllers"
+	"go-phonebooks/middlewares"
 	_ "go-phonebooks/utils/env"
+
+	"regexp"
 
 	"github.com/gorilla/mux"
 )
 
-func HomeRouteHandler(w http.ResponseWriter, r *http.Request) {
-	msg := "Hello world!!!"
-	user := &models.User{
-		Email: "mbapewe@gmail.com",
+func setMiddleware(middlewareName string, handlerFunc http.HandlerFunc) http.Handler {
+	if middlewareName == "jwt" {
+		return middlewares.JwtAuthMiddleware(handlerFunc)
+	} else if middlewareName == "hello" {
+		return middlewares.HelloMiddleware(handlerFunc)
 	}
-	err, isValid := user.Validate()
-	fmt.Println(err)
-	fmt.Println(isValid)
-	if err != nil {
-		res.RespondError(w, 422, "Invalid request!", err)
+	return nil
+}
+
+func appendMiddleware(middlewareName string, otherMiddlewares http.Handler) http.Handler {
+	if middlewareName == "jwt" {
+		return middlewares.JwtAuthMiddleware(otherMiddlewares)
+	} else if middlewareName == "hello" {
+		return middlewares.HelloMiddleware(otherMiddlewares)
+	}
+	return otherMiddlewares
+}
+
+func recursiveMiddleware(myMiddleware http.Handler, middlewareArr []string, start int, handler http.HandlerFunc) http.Handler {
+	if start < len(middlewareArr) {
+		if myMiddleware == nil {
+			myMiddleware = setMiddleware(middlewareArr[start], handler)
+		} else {
+			myMiddleware = appendMiddleware(middlewareArr[start], myMiddleware)
+		}
+		return recursiveMiddleware(myMiddleware, middlewareArr, start+1, handler)
+	}
+	return myMiddleware
+}
+
+func registerRoute(r *mux.Router, ctrl *controllers.Controller, name string, handler func(w http.ResponseWriter, r *http.Request)) {
+	sRoute, ok := ctrl.Routes[name]
+	myRoute := ctrl.PrefixURL
+	regex, _ := regexp.Compile("/$")
+	myRoute = regex.ReplaceAllString(myRoute, "")
+	fmt.Println(myRoute)
+	if sRoute.URL != "" {
+		myRoute = myRoute + sRoute.URL
+	}
+	if !ok {
+		fmt.Println("Cannot find index['" + name + "']")
 		return
 	}
-	res.Respond(w, 200, res.Message(200, msg))
-	// fmt.Println(msg)
+	mids, ok := ctrl.Middlewares[name]
+	if ok {
+		handlerFunc := http.HandlerFunc(handler)
+		enabledMiddlewares := []string{}
+		var midFuncs http.Handler
+		for _, value := range mids {
+			if value == "jwt" || value == "hello" {
+				enabledMiddlewares = append(enabledMiddlewares, value)
+			}
+		}
+		if len(enabledMiddlewares) > 0 {
+			fmt.Println(enabledMiddlewares)
+			midFuncs = recursiveMiddleware(midFuncs, enabledMiddlewares, 0, handlerFunc)
+		}
+		if midFuncs != nil {
+			r.Handle(myRoute, midFuncs).
+				Methods(sRoute.Method).
+				Name(sRoute.Name)
+		} else {
+			r.HandleFunc(myRoute, handler).
+				Methods(sRoute.Method).
+				Name(sRoute.Name)
+		}
+	} else {
+		r.HandleFunc(myRoute, handler).
+			Methods(sRoute.Method).
+			Name(sRoute.Name)
+	}
 }
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/", HomeRouteHandler)
-	router.Use(middlewares.JwtAuthMiddleware)
+	apiRoute := router.PathPrefix("/api").Subrouter()
+	registerRoute(apiRoute, controllers.HomeController, "Index", controllers.HomeController.Index)
+	registerRoute(apiRoute, controllers.HomeController, "Index2", controllers.HomeController.Index2)
+	// router.HandleFunc("/api", controllers.HomeController.Index).
+	// 	Methods("GET")
+	// router.Use(middlewares.JwtAuthMiddleware)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8000"
