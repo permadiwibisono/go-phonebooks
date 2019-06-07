e5c812d72cf0624582124f195ba8231b5db8c293
Permadi Wibisono <mbapewe@gmail.com>
2019-06-07 08:31:38 +0700

    Added HelloMiddleware

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -3,18 +3,20 @@ package middlewares
 import (
 	_ "go-phonebooks/models"
 	"net/http"
+
 	// u "go-phonebooks/utils"
 	// "strings"
 	// jwt "github.com/dgrijalva/jwt-go"
 	// "os"
 	// "context"
-	// "fmt"
+	"fmt"
 )
 
 var JwtAuthMiddleware = func(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// noAuth := []string{"/api/user/new", "/api/user/login"}
 		// requestPath := r.URL.Path
+		fmt.Println("Auth jwt middleware entered!!")
 		next.ServeHTTP(w, r)
 		// for _, value := range noAuth {
 		// 	if value == requestPath {
@@ -22,5 +24,11 @@ var JwtAuthMiddleware = func(next http.Handler) http.Handler {
 		// 		return
 		// 	}
 		// }
+	})
+}
+var HelloMiddleware = func(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Hello middleware entered!!")
+		next.ServeHTTP(w, r)
 	})
 }
